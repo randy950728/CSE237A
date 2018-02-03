ada652df516427880b91b814e7f582663cc41d67
Unknown <randy950728@yahoo.com.tw>
2018-02-03 15:27:04 -0800

    push

--- indv_project/part_2/section1/assignment1.c
+++ indv_project/part_2/section1/assignment1.c
@@ -12,11 +12,97 @@
 void program_init(SharedVariable* sv) {
 	// You can initialize the shared variable if needed.
 	sv->bProgramExit = 0;
+    pinMode(PIN_ALED, OUTPUT);
+    digitalWrite(PIN_ALED, HIGH);
 
+    //Set DIP LED Pin Mode
+    softPwmCreate(PIN_DIP_RED, 0, 0xFF);
+    softPwmCreate(PIN_DIP_GRN, 0, 0xFF);
+    softPwmCreate(PIN_DIP_BLU, 0, 0xFF);
+
+    //Set SMD LED Pin Mode
+    softPwmCreate(PIN_SMD_RED, 0, 0xFF);
+    softPwmCreate(PIN_SMD_GRN, 0, 0xFF);
+    softPwmCreate(PIN_SMD_BLU, 0, 0xFF);
+
+    //Set senors Pin Mode
+    pinMode(PIN_BIG, INPUT);
+    pinMode(PIN_SMALL, INPUT);
+    pinMode(PIN_BUTTON, INPUT);
 	// You also need to initalize sensors here
 }
 
 void program_body(SharedVariable* sv) {
+    static int state=1;
+    int button_val=0;
+    int small_val=0;
+    int big_val=0;
+    button_val = digitalRead(PIN_BUTTON);
+    small_val  = digitalRead(PIN_SMALL);
+    big_val    = digitalRead(PIN_BIG);
+
+    if(button_val == 1)
+    {
+        state = 1-state;
+    }
+
+    if(state==0)
+    {
+        softPwmWrite(PIN_SMD_RED,0);
+        softPwmWrite(PIN_SMD_GRN,0);
+        softPwmWrite(PIN_SMD_BLU,0);
+
+        softPwmWrite(PIN_DIP_RED,0);
+        softPwmWrite(PIN_DIP_GRN,0);
+        softPwmWrite(PIN_DIP_BLU,0);
+
+        digitalWrite(PIN_ALED, LOW);
+    }
+
+    else
+    {
+        if(small_val==1)
+        {
+            softPwmWrite(PIN_DIP_RED,0x00);
+            softPwmWrite(PIN_DIP_GRN,0x00);
+            softPwmWrite(PIN_DIP_BLU,0xFF);
+        }
+
+        else
+        {
+            softPwmWrite(PIN_DIP_RED,0xFF);
+            softPwmWrite(PIN_DIP_GRN,0x00);
+            softPwmWrite(PIN_DIP_BLU,0x00);
+        }
+
+        if(small_val==0 big_val==0)
+        {
+            softPwmWrite(PIN_SMD_RED,0xFF);
+            softPwmWrite(PIN_SMD_GRN,0x00);
+            softPwmWrite(PIN_SMD_BLU,0x00);
+        }
+
+        else if(small_val==1 big_val==0)
+        {
+            softPwmWrite(PIN_SMD_RED,0xEE);
+            softPwmWrite(PIN_SMD_GRN,0x00);
+            softPwmWrite(PIN_SMD_BLU,0xC8);
+        }
+
+        else if(small_val==0 big_val==1)
+        {
+            softPwmWrite(PIN_SMD_RED,0x80);
+            softPwmWrite(PIN_SMD_GRN,0xFF);
+            softPwmWrite(PIN_SMD_BLU,0x00);
+        }
+
+        else if(small_val==1 big_val==1)
+        {
+            softPwmWrite(PIN_SMD_RED,0x00);
+            softPwmWrite(PIN_SMD_GRN,0xff);
+            softPwmWrite(PIN_SMD_BLU,0xff);
+        }
+    }
     // Implement your sensor handling procedure.
     // Keep also this in mind:
     // - Make this fast and efficient.
@@ -31,5 +117,6 @@ void program_body(SharedVariable* sv) {
 void program_exit(SharedVariable* sv) {
     // Clean up everything if needed.
     // This is called when the program finishes.
+    digitalWrite(PIN_ALED, LOW);
 }
 
