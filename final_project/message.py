4d95ac8a788e6c98b318f5c27947689b3d817e77
Unknown <randy950728@yahoo.com.tw>
2018-02-28 16:24:06 -0800

    for later

---
+++ final_project/message.py
@@ -0,0 +1,5 @@
+class message:
+	def __init__(self):
+		self.num_guest=0
+		self.label=[]
+		self.photo=[]
---
+++ final_project/receiver.py
@@ -0,0 +1,7 @@
+while 1:
+    #accept connections from outside
+    (clientsocket, address) = serversocket.accept()
+    #now do something with the clientsocket
+    #in this case, we'll pretend this is a threaded server
+    ct = client_thread(clientsocket)
+    ct.run()
