7d39dc52016d3aeda4c4aadbd34d21446cf6a2b0
Unknown <randy950728@yahoo.com.tw>
2018-02-28 16:52:53 -0800

    test?

--- udp_receiver.py
+++ udp_receiver.py
@@ -1,11 +1,20 @@
 import socket
+import numpy as np
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
  
 sock = socket.socket(socket.AF_INET, # Internet
                       socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
- 
+message = str()
 while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    data, addr = sock.recvfrom(2048) # buffer size is 1024 bytes
+    while(len(data)!=0):
+    	message+=data
+    	data, addr = sock.recvfrom(2048)
+    mat = np.fromstring(message, np.unint8)
+    image = cv2.imdecode(mat, cv2.IMREAD_COLOR)
+    cv2.imshow("frame",image)
+    cv2.waitKey(5000)
+    message=str()
     print "received message:", data
--- udp_sender.py
+++ udp_sender.py
@@ -4,6 +4,7 @@ import cv2
 import sys
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
+pack_size= 2048
 MESSAGE = "Hello, World!"
  
 print "UDP target IP:", UDP_IP
@@ -15,5 +16,14 @@ sock = socket.socket(socket.AF_INET, # Internet
 
 image = cv2.imread("0.jpg")
 MESSAGE = cv2.imencode('.png', image)[1].tostring()
-print "image size", sys.getsizeof(MESSAGE)
-#sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
+total_size = sys.getsizeof(MESSAGE)
+num_packet = total_size/pack_size
+off_set = 0
+for i in range(num_packet):
+	bytes_sent = sock.sendto(MESSAGE[off_set:(off_set+pack_size)], (UDP_IP, UDP_PORT))
+	off_set +=bytes_sent
+	total_size-=bytes_sent
+if(total_size!=0):
+	sock.sendto(MESSAGE[off_set:], (UDP_IP, UDP_PORT))
+
